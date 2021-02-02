82f1abdce12eaab0356a6a728caaf6308c34e217
Safin Singh <safin.singh@gmail.com>
2021-02-02 01:00:17 -0800

    feat: add next config type and test out webpack 5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const { resolve } = require('path')
 
+/** @type {import('next/dist/next-server/server/config').NextConfig} */
 module.exports = {
 	webpack: (config) => {
 		config.resolve.alias['~'] = resolve('./src')
 		config.resolve.alias['~/api'] = resolve('./pages/api')
 		return config
+	},
+	future: {
+		webpack5: true
 	}
 }
